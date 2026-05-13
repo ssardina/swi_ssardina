3bd9827dddcfb9282e6434fccb4556b5ca36ced9
ssardina <ssardina@gmail.com>
2026-05-13 14:50:14 +1000

    upgrade version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(utils_ssardina).
 title('Shared utilities for my Prolog projects').
-version('1.0.0').
+version('1.0.1').
 author('Sebastian Sardina', 'ssardina@gmail.com').
 home('https://github.com/ssardina/swi-utils').
 download('https://github.com/ssardina/swi-utils').
